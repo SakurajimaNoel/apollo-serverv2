3b9db5f4d81d7eb6d72c9c8a4afcdfd355ca3cf9
Amit Koranga <amitkoranga2@gmail.com>
2024-10-13 16:17:08 +0530

    Comment Content

--- src/typedefs.js
+++ src/typedefs.js
@@ -16,24 +16,32 @@ type User {
 type Post{
     id: ID! @id
     caption: String
-    content: String!
+    content: [String]
     likes: Int!
     createdBy: User! @relationship(type: "HAS_POSTED", direction: IN)
     createdOn: DateTime! @timestamp(operations: [CREATE])
     comments: [Comment!]! @relationship(type: "COMMENT_ON", direction: IN)
+    likedBy: [User!]! @relationship(type: "LIKED_BY", direction: OUT)
 }
 
 type Comment{
     id: ID! @id
-    content: String!
+    content: [CommentContent]!
     createdOn: DateTime! @timestamp(operations: [CREATE])
     commentBy: User! @relationship(type: "COMMENT_BY", direction: OUT)
     commentOn: Post! @relationship(type: "COMMENT_ON", direction: OUT)
 }
 
+input CommentContent {
+    text: String!
+    data: String
+}
+
+
 interface IsFriend @relationshipProperties{
     status: FriendshipStatus!
     requestedBy: ID!
+    addedOn: DateTime! @timestamp(operations: [CREATE])
 }
 
 enum FriendshipStatus{
